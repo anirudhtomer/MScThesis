305bc1a73f3203c6a18c11936f85d2e86f109a0f
Anirudh Tomer <anirudhtomer@gmail.com>
2015-10-11 01:06:49 +0200

    small section on bayesian...last work before i sleep

--- latex/code/misc.R
+++ latex/code/misc.R
@@ -1,12 +1,19 @@
 install.packages("ggplot2")
 library(ggplot2)
 
+#mixture density
 mat=rmultinom(1, 10000000, c(1/6,1/2,1/3))
 first=rnorm(mat[1,1], -10, 3)
 second=rnorm(mat[2,1], 0, 1)
 third=rnorm(mat[3,1], 4, 2)
-sample = c(rpois(n = mat[1,1],1), rpois(n = mat[2,1],50), rpois(n = mat[3,1],200))
+sample = c(first, second, third)
 
-densityplot = ggplot()+ aes(sample) + geom_density() + ggtitle("Mixture density of Y")
-densityplot + ylab(expression("f"[ Y ]*"(y)"))  + xlab("Y") + theme(axis.text=element_text(size=14),axis.title=element_text(size=18), plot.title=element_text(size=20))
-geom_segment(aes(x=-10, y=0, xend=-10, yend=max(first)))
+densityplot = ggplot()+ aes(sample) + geom_density()
+densityplot + ylab(expression("p"[ Y ]*"(y)"))  + xlab("Y") + theme(axis.text=element_text(size=14),axis.title=element_text(size=18), plot.title=element_text(size=20))
+
+#toy problem 
+sample = rbeta(n=100000, shape1 = 9, shape2 = 3)
+densityplot = ggplot()+ aes(sample) + geom_density()
+densityplot + ylab(expression("p("*pi*")"))  + xlab(expression(pi)) + theme(axis.text=element_text(size=14),axis.title=element_text(size=18), plot.title=element_text(size=20))
+
+
